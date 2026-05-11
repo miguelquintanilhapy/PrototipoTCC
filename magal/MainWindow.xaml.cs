fbb308dafb7d3534a810839646d84b575ae02146
miguelquintanilhapy <miguelquintanilha61@gmail.com>
2026-05-11 16:44:23 -0300

    Finalizando tela de home

--- magal/MainWindow.xaml.cs
+++ magal/MainWindow.xaml.cs
@@ -8,53 +8,76 @@ namespace magal
 {
     public partial class MainWindow : Window
     {
+        // Cache das Views para melhorar a performance (Navegação Instantânea)
+        private HomeView _homeView;
+        private HistoricoView _historicoView;
+        private OrcamentoView _orcamentoView;
+
         public MainWindow()
         {
             InitializeComponent();
             this.WindowState = WindowState.Maximized;
 
-            AbrirOrcamento();
+            // Inicia o sistema na Home
+            AbrirHome();
         }
 
-        // Evento do botão "Novo Orçamento"
+        private void BtnHome_Click(object sender, RoutedEventArgs e)
+        {
+            AbrirHome();
+        }
+
         private void BtnOrcamentos_Click(object sender, RoutedEventArgs e)
         {
             AbrirOrcamento();
         }
-
 
         private void BtnHistorico_Click(object sender, RoutedEventArgs e)
         {
             AbrirHistorico();
         }
 
-        private void AbrirOrcamento()
+        public void AbrirHome()
         {
+            if (_homeView == null)
+                _homeView = new HomeView();
 
-            MainContent.Content = new OrcamentoView();
+            MainContent.Content = _homeView;
         }
 
-        private void AbrirHistorico()
+        public void AbrirOrcamento()
         {
-            MainContent.Content = new HistoricoView();
+            // Criamos uma nova instância se quisermos um orçamento "limpo" 
+            // ou mantemos o cache se quiser salvar o que o usuário estava digitando
+            if (_orcamentoView == null)
+                _orcamentoView = new OrcamentoView();
+
+            MainContent.Content = _orcamentoView;
+        }
+
+        public void AbrirHistorico()
+        {
+            // No Histórico é bom recriar ou atualizar para pegar novos dados do banco
+            // Mas mantendo o cache, a troca de tela é instantânea
+            if (_historicoView == null)
+                _historicoView = new HistoricoView();
+
+            MainContent.Content = _historicoView;
         }
 
         public void IrParaEdicao(Projeto projetoSimplificado)
         {
             var repo = new ProjetoRepository();
-            // Busca tudo do banco (tarefas, custos, orçamento) usando o ID
             Projeto projetoCompleto = repo.CarregarProjetoCompleto(projetoSimplificado.id_projeto);
 
             var viewModel = new OrcamentoViewModel();
-
-
-            // Passa o projeto completo para a VM
             viewModel.CarregarProjetoParaEdicao(projetoCompleto);
 
-            // Troca de tela
             var view = new OrcamentoView();
             view.DataContext = viewModel;
             MainContent.Content = view;
         }
+
+        
     }
 }
---
+++ magal/Views/HomeView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace magal.Views
+{
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
